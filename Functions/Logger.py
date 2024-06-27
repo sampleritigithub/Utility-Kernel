d0ea04dd208d46688d3d427fd3953833eef0db07
Tobey_Source <103457750+sampleritigithub@users.noreply.github.com>
2024-06-27 02:52:56 -0700

    Delete Functions/Logger.py

--- Functions/Logger.py
+++
@@ -1,6 +0,0 @@
-from Functions import printingSystem
-from Functions import writeFiles
-from Functions import timeSignature
-def LOG(level="INFO", text="logger called",user="defaultKernelBody"):
-    printingSystem.Print(f"Logger Called [by {user}] -- Data: {text} -- {level} level",color="GREEN",programName="kernel.Logger")
-    writeFiles.AppendASCII(f"[{user} {timeSignature.Signature()}] {level}: {text} | end |      ",f"{user}.log")
